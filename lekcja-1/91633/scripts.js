3b57e9106db3b124a0b55409e7aacc41ae71ece7
Daniel Subatowicz <danielius2005m@gmail.com>
2025-11-04 17:15:59 +0200

    zad2

---
+++ lekcja-1/91633/scripts.js
@@ -0,0 +1,21 @@
+// Basic console outputs
+console.log("Hello, world!");
+console.log(42);
+let myVariable = "This is a variable";
+console.log(myVariable);
+
+// Variables and Data Types
+let name = "Oskar";
+const age = 20;
+let isStudent = true;
+console.log(name, age, isStudent);
+
+// Basic function
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log("The sum is:", addNumbers(5, 7));
+
+// DOM Manipulation
+document.getElementById("main-title").textContent = "Welcome to My Awesome Webpage!";
+document.querySelector(".highlight").style.backgroundColor = "lightcoral";
