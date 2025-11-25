5fff57b92550b90d3bb49b088cea997b0db36dc7
cerniakdaniel <cerniakdaniel@gmail.com>
2025-11-25 19:08:42 +0200

    Daniel_Cer_Tasks

---
+++ tasks-done/Daniel_Czerniak_gr1/Zadanie 1/Zadanie1.js
@@ -0,0 +1,22 @@
+console.log("Hello from JavaScript!");
+console.log(42);
+let greeting = "Welcome!";
+console.log(greeting);
+
+
+let numberValue = 10;
+const booleanValue = true;
+let stringValue = "Sample string";
+
+
+function sum(a, b) {
+return a + b;
+}
+console.log("Sum:", sum(5, 7));
+
+
+let textElement = document.getElementById("text-change");
+textElement.textContent = "This text has been changed by JavaScript!";
+
+
+document.querySelector("h2").style.color = "green";
